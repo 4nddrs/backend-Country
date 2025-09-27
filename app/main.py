fda51b46c5bf068031a8cfe59b8a8319322d02ba
MishelBravo <bam0032019@est.univalle.edu>
2025-09-27 19:14:24 -0400

    .

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ app = FastAPI(title="backend-Country-API")
 # ⚡ Middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],  # Permite cualquier dominio (para pruebas)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
